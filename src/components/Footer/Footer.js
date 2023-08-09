4993595604eb1d88ce195ae4b2fe1b2665c0d946
Ice <aiswariya2324@gmail.com>
2023-08-09 15:52:27 +0530

    Loading the Existing Files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,36 @@
+const header = {
+  homepage: 'http://localhost:3000/',
+  title: 'JS.',
+}
+
+const about = {
+  name: 'Aisw@riya',
+  role: 'Data Scientist',
+  description:
+    'I in-times Make Computers Think !',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com/aiswariya2324',
+  },
+}
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Python',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Jest',
+  'Enzyme',
+]
+
+const contact = {
+  email: 'ice@gmail.com',
+}
+
+export { header, about, skills, contact }
